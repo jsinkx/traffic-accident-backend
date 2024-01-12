76e43d1eb1f0e02ce7f6eb02efa5de2ffa55167f
Artemiy <andrikv@gmail.com>
2024-01-12 20:08:02 +0300

    Added predict_proba in create_forecast.py

--- controllers/create_forecast.py
+++ controllers/create_forecast.py
@@ -1,12 +1,22 @@
+# import lime
+# import lime.lime_tabular
 from models.models import models
 
 def create_forecast_accident(model_id, data):
-    # Если модель не найдена, берется первая модель из всех
-    model = (list(filter(lambda v: v['id'] == model_id, models)) or models)[0]
+        # Если модель не найдена, берется первая модель из всех
+        model = (list(filter(lambda v: v['id'] == model_id, models)) or models)[0]
+
+        # todo: добавить локальную интерпретацию случая
+
+        # explainer = lime.lime_tabular.LimeTabularExplainer(data.values, feature_names=data.columns.values.tolist(), class_names=['accident', 'no accident'], mode='classification')
+        # exp = explainer.explain_instance(data.values[0], model['model'].predict_proba, num_features=len(data.columns))
     
-    return {
-            'model_name': model['name'], 
-            'model_ru_name': model['ru_name'], 
-            'forecasted_value': int(model['model'].predict(data)),
-            'success': True 
-    }        
+        # exp.save_to_file('./cache/lime.html')
+        
+        return {
+                'model_name': model['name'], 
+                'model_ru_name': model['ru_name'], 
+                'predicted_class': int(model['model'].predict(data)),
+                'predicted_probabilities': model['model'].predict_proba(data).tolist(), # [вероятность_класс_0, вероятность_класс_1]
+                'success': True 
+        }        
